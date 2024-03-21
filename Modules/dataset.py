39d23d99ec5dfd78eccc428d905762668a11a686
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-03-21 21:21:40 +0900

    .

--- Modules/dataset.py
+++ Modules/dataset.py
@@ -9,17 +9,17 @@ class CustomDataset(Dataset):
         self.window_size = 5 * 4
 
     def __len__(self):
-        return len(self.data) - self.window_size - 5
+        return len(self.data) - self.window_size - 1
 
     def __getitem__(self, index):
         start_index = index
         end_index = index + self.window_size
 
-        if end_index + 5 > len(self.data):
+        if end_index + 1 > len(self.data):
             raise IndexError("Index out of bounds. Reached the end of the dataset.")
 
         X_train = self.data.iloc[start_index:end_index, :]
-        y_train = self.data.iloc[end_index:end_index + 5, 0]
+        y_train = self.data.iloc[end_index:end_index + 1, 0]
 
         X_train_tensor = torch.Tensor(X_train.values)
         y_train_tensor = torch.Tensor(y_train.values)
--- Modules/model.py
+++ Modules/model.py
@@ -41,50 +41,50 @@ class StackedLSTM(nn.Module):
         # 출력을 위한 선형 레이어
         out = self.fc(out)
 
-        out = out[:, :5, :]
+        out = out[:, :1, :]
 
         return out
 
 
-class StackedGRU(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, bidirectional):
-        super(StackedGRU, self).__init__()
-
-        if bidirectional:
-            hidden_size2 = hidden_size * 2
-
-        if not bidirectional:
-            hidden_size2 = hidden_size
-
-        # 첫 번째 GRU 층
-        self.GRU1 = nn.GRU(input_size, hidden_size, num_layers=num_layers,
-                           bidirectional=bidirectional, dropout=0.4, batch_first=True)
-
-        # 두 번째 GRU 층
-        self.GRU2 = nn.GRU(hidden_size2, hidden_size, num_layers=num_layers,
-                           bidirectional=bidirectional, dropout=0.3, batch_first=True)
-
-        # 세 번째 GRU 층
-        self.GRU3 = nn.GRU(hidden_size2, hidden_size, num_layers=num_layers,
-                           bidirectional=bidirectional, dropout=0.1, batch_first=True)
-
-        # 출력을 위한 선형 레이어
-        self.fc = nn.Linear(hidden_size2, output_size)
-
-    def forward(self, x):
-        # 첫 번째 GRU 층
-        out, _ = self.GRU1(x)
-
-        # 두 번째 GRU 층
-        out, _ = self.GRU2(out)
-
-        # 세 번째 GRU 층
-        out, _ = self.GRU3(out)
-
-        # 출력을 위한 선형 레이어
-        out = self.fc(out)
-
-        return out
+# class StackedGRU(nn.Module):
+#     def __init__(self, input_size, hidden_size, num_layers, output_size, bidirectional):
+#         super(StackedGRU, self).__init__()
+#
+#         if bidirectional:
+#             hidden_size2 = hidden_size * 2
+#
+#         if not bidirectional:
+#             hidden_size2 = hidden_size
+#
+#         # 첫 번째 GRU 층
+#         self.GRU1 = nn.GRU(input_size, hidden_size, num_layers=num_layers,
+#                            bidirectional=bidirectional, dropout=0.4, batch_first=True)
+#
+#         # 두 번째 GRU 층
+#         self.GRU2 = nn.GRU(hidden_size2, hidden_size, num_layers=num_layers,
+#                            bidirectional=bidirectional, dropout=0.3, batch_first=True)
+#
+#         # 세 번째 GRU 층
+#         self.GRU3 = nn.GRU(hidden_size2, hidden_size, num_layers=num_layers,
+#                            bidirectional=bidirectional, dropout=0.1, batch_first=True)
+#
+#         # 출력을 위한 선형 레이어
+#         self.fc = nn.Linear(hidden_size2, output_size)
+#
+#     def forward(self, x):
+#         # 첫 번째 GRU 층
+#         out, _ = self.GRU1(x)
+#
+#         # 두 번째 GRU 층
+#         out, _ = self.GRU2(out)
+#
+#         # 세 번째 GRU 층
+#         out, _ = self.GRU3(out)
+#
+#         # 출력을 위한 선형 레이어
+#         out = self.fc(out)
+#
+#         return out
 
 
 class RegressionModel(nn.Module):
@@ -97,8 +97,12 @@ class RegressionModel(nn.Module):
                                     output_size=output_size, bidirectional=bidirectional)
 
         if additional:
-            self.additional_layer = StackedGRU(input_size=output_size, hidden_size=hidden_size, num_layers=num_layers,
-                                               output_size=output_size, bidirectional=bidirectional)
+            self.additional_layer = nn.Sequential(
+                nn.Linear(output_size, hidden_size),
+                nn.GELU(),
+                nn.Dropout(0.1),
+                nn.Linear(hidden_size, output_size),
+            )
 
     def forward(self, train, gt=None):
         output = self.backbone(train)
