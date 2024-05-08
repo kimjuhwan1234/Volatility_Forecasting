97ac3718977cf6e8e716ff6062fec2fc4e0c81dc
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-05-08 22:43:36 +0900

    .

--- utils/parser.py
+++ utils/parser.py
@@ -2,7 +2,7 @@ import argparse
 from Modules.model import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input_size", type=int, default=2, help="input_size")
+parser.add_argument("--input_size", type=int, default=6, help="input_size")
 parser.add_argument("--hidden_size", type=int, default=64, help="hidden_size")
 parser.add_argument("--num_layers", type=int, default=2, help="num_layers")
 parser.add_argument("--output_size", type=int, default=1, help="output_size")
