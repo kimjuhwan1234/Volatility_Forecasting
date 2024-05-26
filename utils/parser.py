ed2437e610f42d54a9af9f9ed9a4c140c8307824
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-05-26 23:51:55 +0900

    .

--- utils/parser.py
+++ utils/parser.py
@@ -2,13 +2,13 @@ import argparse
 from Modules.model import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input_size", type=int, default=1, help="input_size")
+parser.add_argument("--input_size", type=int, default=3, help="input_size")
 parser.add_argument("--hidden_size", type=int, default=64, help="hidden_size")
 parser.add_argument("--num_layers", type=int, default=2, help="num_layers")
 parser.add_argument("--output_size", type=int, default=1, help="output_size")
 
-parser.add_argument("--retrain", type=bool, default=False, help="Retrain")
-parser.add_argument("--Transfer", type=bool, default=False, help="Backbone 훈련이면 False 아니면 True")
+parser.add_argument("--retrain", type=bool, default=True, help="Retrain")
+parser.add_argument("--Transfer", type=bool, default=True, help="Backbone 훈련이면 False 아니면 True")
 parser.add_argument("--additional", type=bool, default=False, help="additional")
 parser.add_argument("--bidirectional", type=bool, default=True, help="bidirectional")
 
@@ -23,7 +23,7 @@ parser.add_argument("--model_saving_strategy", type=str, default='better', help=
 parser.add_argument("--seed", type=int, default=0, help="seed")
 parser.add_argument("--num_workers", type=int, default=4, help="num_workers")
 parser.add_argument("--device", type=str, default='cuda', help="device")
-parser.add_argument("--epochs", type=int, default=50, help="epochs")
+parser.add_argument("--epochs", type=int, default=300, help="epochs")
 parser.add_argument("--batch_size", type=int, default=32, help="batch_size")
 parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
 parser.add_argument("--patience", type=int, default=10, help="patience")
@@ -32,7 +32,7 @@ parser.add_argument("--use_wandb", type=bool, default=False, help="use_wandb")
 
 parser.add_argument("--backbone_val_end", type=str, default='2005-01-01', help="date")
 parser.add_argument("--transfer_test_end", type=str, default='2024-01-01', help="date")
-parser.add_argument("--backbone_weight_path", type=str, default='Weight/Backbone/BiLSTM_SP.pth', help="weight")
+parser.add_argument("--backbone_weight_path", type=str, default='Weight/Backbone/BiLSTM_BZ_ES.pth', help="weight")
 
 opt_train = parser.parse_args()
 print(opt_train)
